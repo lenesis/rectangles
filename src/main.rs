1691e454b3ab59df4a1d4c3c47563ad03af690c2
Lenesis <lenesis@proton.me>
2024-10-07 04:39:57 -0400

    re-writing using tuples

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "The area of this rectangle is {} square pixels.",
-        area(width1, height1)
-    );
+    let rectangle: (u32, u32) = (30, 50);
+    
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
-}
