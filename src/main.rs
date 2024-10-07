4ebab7fda489ab9164125a767686217fcd38843b
Lenesis <lenesis@proton.me>
2024-10-07 05:29:48 -0400

    having some fun!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -14,8 +15,19 @@ fn main() {
         area(&rect1)
     );
 
+    //let's have some fun!
+    // println!("rect1 is {}", rect1); //! Rectangle` doesn't implement `std::fmt::Display
+    //? structs don’t have a provided implementation of Display to use with println! and the {} placeholder.
+    //Trying again...
+    //println!("rect1 is {:?}", rect1); //! error[E0277]: `Rectangle` doesn't implement `Debug`
+    //? add `#[derive(Debug)]` to `Rectangle` or manually `impl Debug for Rectangle`
+
+    //* Last try...
+    println!("rect1 is {:#?}", rect1); //* rect1 is Rectangle { width: 30, height: 50 }
+    
+
 }
 
-fn area (rectangle: &Rectangle) -> u32 {
+fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
