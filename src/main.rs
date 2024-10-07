a1ccca286348d08b2082d698e7ac0c72809d7d02
Lenesis <lenesis@proton.me>
2024-10-07 05:14:28 -0400

    area() added, using structs

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,13 @@ fn main() {
         height: 50,
     };
 
-    
+    println!(
+        "The area of this rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+}
+
+fn area (rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
