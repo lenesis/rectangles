82bf1012052b0c5e6455b1030001a980bf69ad2f
Lenesis <lenesis@proton.me>
2024-10-07 04:44:05 -0400

    added area(), this time using tuples

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     let rectangle: (u32, u32) = (30, 50);
-    
+    println!(
+        "the area of this rectangle is {} square pixels.",
+        area(rectangle)
+    );
 }
 
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
